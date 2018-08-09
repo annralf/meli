0b55e73a8fba350bc4cbb737f02529c6c3e9b3dc
annralf <anaguere@gmail.com>
2018-08-08 21:26:12 -0400

    Modulando acceso a token meli

--- resources/auth.php
+++ resources/auth.php
@@ -2,12 +2,12 @@
 require_once 'db_mng.php';
 
 $conn = new Connect();
+$tokenMng = new MELIConnect(1)
 
 if (isset($_POST['code'])) {
     $authorization_code = $_POST['code'];
-	
+    $result = $tokenMng->search_access_token($authorization_code);
 }else{
-    $tokenMng = new MELIConnect(1);
     $application_id = $tokenMng->app_detail->application_id;
     $url = "http://auth.mercadolibre.com.co/authorization?response_type=code&client_id=$application_id";
     header("Location:".$url);
