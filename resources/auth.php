2b244707f2ba6f2a62968b339406d3978734d815
annralf <anaguere@gmail.com>
2018-08-23 16:37:04 -0400

    auth testing

--- resources/auth.php
+++ resources/auth.php
@@ -11,8 +11,9 @@ if (isset($_POST['code'])) {
 	$application_id = $tokenMng->app_detail->application_id;
 	$application_secret_key = $tokenMng->app_detail->secret_key;
 	$url = "https://api.mercadolibre.com/oauth/token?grant_type=authorization_code&client_id=$application_id&client_secret=$application_secret_key&code=$code&redirect_uri=https://app.tokioexpress.co/resources/auth.php";
-	 $connection = $tokenMng->app_detail->CURLRequest($url, 'POST',NULL);
-	 echo $tokenMng->app_detail->set_access_token($connection->access_token, $connection->refresh_token);
+	echo $url;die();
+	$connection = $tokenMng->app_detail->CURLRequest($url, 'POST',NULL);
+	echo $tokenMng->app_detail->set_access_token($connection->access_token, $connection->refresh_token);
 
 }else{
 	$application_id = $tokenMng->app_detail->application_id;
