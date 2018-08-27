7775c1e5c924b35f13aa4f1e90681dcec81d2ee4
annralf <anaguere@gmail.com>
2018-08-27 11:41:01 -0400

    integrating refresh access token

--- resources/auth.php
+++ resources/auth.php
@@ -11,6 +11,24 @@ if (isset($_GET['code'])) {
 	$application_id = $tokenMng->app_detail->application_id;
 	$application_secret_key = $tokenMng->app_detail->secret_key;
 	$url = "https://api.mercadolibre.com/oauth/token?grant_type=authorization_code&client_id=$application_id&client_secret=$application_secret_key&code=$authorization_code&redirect_uri=https://app.tokioexpress.co/resources/auth.php";
+	$connection = curl_conn($url);
+	echo $tokenMng->set_access_token($connection->access_token, $connection->refresh_token);
+
+}else{
+	$application_id = $tokenMng->app_detail->application_id;
+	if (isset($tokenMng->app_detail->refresh_access_token)) {
+		$refresh_token = $tokenMng->app_detail->refresh_access_token;
+		$application_secret_key = $tokenMng->app_detail->secret_key;
+		$url = "https://api.mercadolibre.com/oauth/token?grant_type=refresh_token&client_id=$application_id&client_secret=$application_secret_key&refresh_token=$refresh_token";
+		$connection = curl_conn($url);
+		echo $tokenMng->set_access_token($connection->access_token, $connection->refresh_token);
+	}else{
+		$url = "https://auth.mercadolibre.com.co/authorization?response_type=code&client_id=$application_id";
+		header("Location:".$url);	    
+	}
+}
+
+function curl_conn($url){
 	$ch             = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -20,13 +38,5 @@ if (isset($_GET['code'])) {
 	curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 	$connection = json_decode(curl_exec($ch));
 	curl_close($ch);
-	echo $tokenMng->set_access_token($connection->access_token, $connection->refresh_token);
-
-}else{
-	$application_id = $tokenMng->app_detail->application_id;
-	if (isset(var)) {
-		# code...
-	}
-	$url = "https://auth.mercadolibre.com.co/authorization?response_type=code&client_id=$application_id";
-	header("Location:".$url);
+	return $connection;
 }
