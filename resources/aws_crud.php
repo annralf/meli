108a04e542740b976da5ba093afe087c1830d556
annralf <anaguere@gmail.com>
2018-08-27 10:01:24 -0400

    updating offset

--- resources/aws_crud.php
+++ resources/aws_crud.php
@@ -20,7 +20,7 @@ class aws_mng
 	}
 
 	function update(){
-		$sql = "select upper(sku) as sku from aws_sku order by id desc offset 3300;";
+		$sql = "select upper(sku) as sku from aws_sku order by id desc offset 3670;";
 		$result = pg_query($sql);
 		$list = array();
 		$cant = 0;
