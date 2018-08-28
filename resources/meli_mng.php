a4c0a5c975fe3aaa6832fe1f3a5b63f9a77d00a8
annralf <anaguere@gmail.com>
2018-08-27 21:34:35 -0400

    funciones de creación ok

--- resources/meli_mng.php
+++ resources/meli_mng.php
@@ -130,11 +130,13 @@ class Meli
 			$buying_mode = (array_search('buy_it_now', $category_info->settings->buying_modes)) ? array_search('buy_it_now', $category_info->settings->buying_modes): array_search('classified', $category_info->settings->buying_modes);
 			$currency = array_search('COP', $category_info->settings->currencies);
 			$category_info = array(
-			    'category_id' => $category_info->id,
-			    'buying_mode' => $category_info->settings->buying_modes[$buying_mode],
-			    'shipping_mode' => $category_info->settings->shipping_modes[$shipping_mode],
-			    'currency' => $category_info->settings->currencies[$currency],
-			    'domain' => $category_info->settings->vip_subdomain
+				'category_id' => $category_info->id,
+				'buying_mode' => $category_info->settings->buying_modes[$buying_mode],
+				'shipping_mode' => $category_info->settings->shipping_modes[$shipping_mode],
+				'currency' => $category_info->settings->currencies[$currency],
+				'domain' => $category_info->settings->vip_subdomain,
+				'max_title_length' => $category_info->settings->max_title_length,
+				'max_description_length' => $category_info->settings->max_description_length
 			);
 			return $category_info;
 		}
@@ -221,7 +223,7 @@ class Meli
 			return $validation;
 		}
 		public function update($item_id, $item) {
-			$update_url = "https://api.mercadolibre.com/items/".$item_id."?access_token=".$this->access_token;
+			$update_url = "https://api.mercadolibre.com/items/".$item_id."?access_token=".$this->shop_detail->access_token;
 			$ch         = curl_init();
 			$item       = json_encode($item);
 			curl_setopt($ch, CURLOPT_URL, $update_url);
@@ -236,7 +238,7 @@ class Meli
 		}
 
 		public function relist($item_id, $item) {
-			$update_url = "https://api.mercadolibre.com/items/".$item_id."/relist?access_token=".$this->access_token;
+			$update_url = "https://api.mercadolibre.com/items/".$item_id."/relist?access_token=".$this->shop_detail->access_token;
 			$ch         = curl_init();
 			$item       = json_encode($item);
 			curl_setopt($ch, CURLOPT_URL, $update_url);
@@ -250,7 +252,7 @@ class Meli
 			return $update;
 		}
 		public function create($item) {
-			$show_url = "https://api.mercadolibre.com/items?access_token=".$this->access_token;
+			$show_url = "https://api.mercadolibre.com/items?access_token=".$this->shop_detail->access_token;
 			$ch       = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $show_url);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -262,8 +264,23 @@ class Meli
 			return $show;
 		}
 
+		public function banner($item_id, $item) {
+			$update_url = "https://api.mercadolibre.com/items/".$item_id."/description?access_token=".$this->shop_detail->access_token;
+			$ch         = curl_init();
+			$item       = json_encode($item);
+			curl_setopt($ch, CURLOPT_URL, $update_url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $item);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+			$update = json_decode(curl_exec($ch));
+			curl_close($ch);
+			return $update;
+		}
+
 		public function show($item) {
-			$show_url = "https://api.mercadolibre.com/items?ids=".$item."?access_token=".$this->access_token;
+			$show_url = "https://api.mercadolibre.com/items?ids=".$item."?access_token=".$this->shop_detail->access_token;
 			$ch       = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $show_url);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -276,40 +293,74 @@ class Meli
 		public function newItem(){
 			$this->connect;
 			$id = $this->shop_detail->id;
-			$sql = "SELECT * FROM meli_item_detail WHERE shop_id = $id LIMIT 1;";
+			$sql = "select * from meli_item_detail where id not in (select aws_id from meli_items where shop_id = $id) limit 5";
 			$result = pg_query($sql);
-			$delivery_time = "TIEMPOS DE ENTREGA";
+			$description_title = "DESCRIPCION DEL PRODUCTO";
+			$description_title .= "\n";
+			$delivery_time  = "\n";
 			$delivery_time .= "\n";
-			$delivery_time .= "DE 10 A 15 DIAS HABILES";
+			$delivery_time .= "TIEMPOS DE ENTREGA";
 			$delivery_time .= "\n";
-			$delivery_time .= "&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;&#9620;";
+			$delivery_time .= "• DE 10 A 15 DIAS HABILES";
 			$delivery_time .= "\n";
+			$delivery_time .= "\n";
+			$complementary_description = "ESTE ES UN ARTICULO IMPORTADO DESDE JAPON Ó USA";
+			$complementary_description .= "\n";
+			$complementary_description .= "\n";
+			$complementary_description .="MÉTODOS DE ENVÍO";
+			$complementary_description .= "\n";
+			$complementary_description .= "Nuestros envíos son totalmente gratis y cubren todo el territorio nacional de Colombia a través del correo 4-72";
+			$complementary_description .= "\n";
+			$complementary_description .= "\n";
+			$complementary_description .= "EN CASO DE RETRACTO";
+			$complementary_description .= "\n";
+			$complementary_description .= "• Se puede realizar la devolución del producto en un periodo máximo de 5 días hábiles a partir de la entrega.";
+			$complementary_description .= "\n";
+			$complementary_description .= "• Los costos de retorno hacia los Estados Unidos son asumidos por el COMPRADOR, este varía de acuerdo con el peso y/o volumen del producto y no es reembolsable.";
+			$k = 1;
 			while ($item = pg_fetch_object($result)) {
 				$category_info = $this->search_category($item->category_id);
 				$images      = explode("~^~", $item->pictures);
 				$pictures = array();
 				$i = 0;
 				while ($i < count($images) && $i < 8) {
-				    array_push($pictures, array('source' => $images[$i]));
-				    $i++;					
+					array_push($pictures, array('source' => $images[$i]));
+					$i++;					
 				}
 				$shipping = array();
 				if($category_info['shipping_mode'] == 'me2'){
-				    $shipping = array('mode'    => 'me2', 
-						      'local_pick_up'    => false, 
-						      'free_shipping'    => true ,
-						      'free_methods' => array(),
-						      'tags' => array('mandatory_free_shipping'));
+					$shipping = array('mode'    => 'me2', 
+					                  'local_pick_up'    => false, 
+					                  'free_shipping'    => true ,
+					                  'free_methods' => array(),
+					                  'tags' => array('mandatory_free_shipping'));
 				}else{
-				    $shipping = array('mode'    => 'custom', 
-						      'local_pick_up'    => false, 
-						      'free_shipping'    => false , 
-						      'costs' => array('description' => 'Pagar el Envío en mi Domicilio', 
-						      'cost' => 1));
+					$shipping = array('mode'    => 'custom', 
+					                  'local_pick_up'    => false, 
+					                  'free_shipping'    => false , 
+					                  'costs' => array('description' => 'Pagar el Envío en mi Domicilio', 
+					                                   'cost' => 1));
 				}
 				$avaliable_quantity = ($item->avaliable_quantity == 0) ? 3:$item->avaliable_quantity;
+				$title = $this->scratch->change_simbols($item->title);
+				$description = str_replace(".-", "\n", $this->scratch->change_simbols($item->description));
+				$complementary_description = $delivery_time.$complementary_description;
+				$length = ($category_info['max_description_length'] - strlen($complementary_description)) -1; 
+				$length_title = $category_info['max_title_length'] -1;
+				if (strlen($title) >= $length_title) {
+					$pos   = strrpos($title,' ', $length_title);
+					if ($pos > $length_title) {
+					    $pos -= $pos - $length_title;
+					}
+					$title = substr($title, 0, $pos);
+				}
+				if (strlen($description) >= $length) {
+					$pos   = strpos($description, ' ', $length);
+					$description = substr($description, 0, $pos);
+				}
+				$description = $description_title.$description.$complementary_description;
 				$new_item = array(
-					'title' => $item->title,
+					'title' => $title,
 					'category_id' => $category_info['category_id'],
 					'domain_id' => $category_info['domain'],
 					'price' => $this->set_price($item->weight,$item->price),
@@ -318,28 +369,107 @@ class Meli
 					'buying_mode' => $category_info['buying_mode'],
 					'listing_type_id' => 'gold_special',
 					'condition' => 'new',
-					'description' => $delivery_time.$item->complementary_description,
+					'description' => array('plain_text' => $description),
 					'warranty' => $item->warranty,
 					'pictures' => $pictures,
 					'seller_custom_field' => $item->sku,
 					'shipping' => $shipping
 				);
-				print_r($this->validate($new_item));
-				
-			}
+				if(!is_null($this->validate($new_item))){
+					echo "$k - item no created wrong validation\n";
+				}else{
+					$meli_item = $this->create($new_item);
+					if (isset($meli_item->id)) {
+						$mpid = $meli_item->id;
+						$title = $meli_item->title;
+						$seller_id = $meli_item->seller_id;
+						$category_id = $meli_item->category_id;
+						$price = $meli_item->price;
+						$base_price = $meli_item->base_price;
+						$sold_quantity = $meli_item->sold_quantity;	     
+						$start_time = $meli_item->start_time;
+						$stop_time = $meli_item->stop_time;
+						$permalink = $meli_item->permalink;
+						$status = $meli_item->status;
+						$aws_id = $item->id;
+						$automatic_relist = $meli_item->automatic_relist;
+						$date_created = $meli_item->date_created;
+						$last_updated = $meli_item->last_updated;
+						$shop_id = $this->shop_detail->id;
+						$create_date = date('Y-m-d h:i:s');
+						$update_date = date('Y-m-d h:i:s');
+						$sql = "INSERT INTO meli_items(
+						mpid, title, seller_id, category_id, price, base_price, sold_quantity, 
+						start_time, stop_time, permalink, status, 
+						aws_id, automatic_relist, date_created, last_updated, shop_id, 
+						create_date, update_date) VALUES ('$mpid', '$title', '$seller_id', '$category_id', '$price', '$base_price', '$sold_quantity', '$start_time', '$stop_time', '$permalink', '$status', '$aws_id', '$automatic_relist', '$date_created', '$last_updated', '$shop_id', '$create_date', '$update_date');";
+						$result = pg_query($sql);
+						if ($result > 0) {
+							echo "$k - item $mpid create at $create_date\n";
+						}else{
+							echo "$k - item $mpid not create at DB\n";					    
+						}
+					}else{
+						echo "$k - item no created\n";
+					}
 
+				}
+				$k++;
+			}
 		}
 		public function updateItem(){
 			$this->connect;
 			$id = $this->shop_detail->id;
-			$sql = "SELECT price, avaliable_quantity FROM meli_item_detail WHERE shop_id = $id;";
+			$sql = "SELECT * FROM meli_item_update WHERE shop_id = '$id';";
 			$result = pg_query($sql);
+			$description_title = "DESCRIPCION DEL PRODUCTO";
+			$description_title .= "\n";
+			$delivery_time  = "\n";
+			$delivery_time .= "\n";
+			$delivery_time .= "TIEMPOS DE ENTREGA";
+			$delivery_time .= "\n";
+			$delivery_time .= "• DE 10 A 15 DIAS HABILES";
+			$delivery_time .= "\n";
+			$delivery_time .= "\n";
+			$complementary_description = "ESTE ES UN ARTICULO IMPORTADO DESDE JAPON Ó USA";
+			$complementary_description .= "\n";
+			$complementary_description .= "\n";
+			$complementary_description .="MÉTODOS DE ENVÍO";
+			$complementary_description .= "\n";
+			$complementary_description .= "Nuestros envíos son totalmente gratis y cubren todo el territorio nacional de Colombia a través del correo 4-72";
+			$complementary_description .= "\n";
+			$complementary_description .= "\n";
+			$complementary_description .= "EN CASO DE RETRACTO";
+			$complementary_description .= "\n";
+			$complementary_description .= "• Se puede realizar la devolución del producto en un periodo máximo de 5 días hábiles a partir de la entrega.";
+			$complementary_description .= "\n";
+			$complementary_description .= "• Los costos de retorno hacia los Estados Unidos son asumidos por el COMPRADOR, este varía de acuerdo con el peso y/o volumen del producto y no es reembolsable.";
+
 			while ($item = pg_fetch_object($result)) {
-				$new_item = array(
+				$category_info = $this->search_category($item->category_id);
+				$avaliable_quantity = ($item->avaliable_quantity == 0) ? 3:$item->avaliable_quantity;
+				$title = $this->scratch->change_simbols($item->title);
+				$description = str_replace(".-", "\n", $this->scratch->change_simbols($item->description));
+				$complementary_description = $delivery_time.$complementary_description;
+				$length = ($category_info['max_description_length'] - (strlen($complementary_description) + strlen($description_title))) -1; 
+				$length_title = $category_info['max_title_length'] -1;
+				if (strlen($title) >= $length_title) {
+					$pos   = strpos($title,' ', $length_title);
+					$title = substr($title, 0, $pos);
+				}
+				if (strlen($description) >= $length) {
+					$pos   = strpos($description, ' ', $length);
+					$description = substr($description, 0, $pos);
+				}
+				$description = $description_title.$description.$complementary_description;
+				$update_item = array(
+					'title' => $title,
 					'price' => $this->set_price($item->weight,$item->price),
-					'available_quantity' => $item->avaliable_quantity,
+					'available_quantity' => $avaliable_quantity
 				);
-				print_r($new_item);
+				$update = $this->banner($item->mpid,array('plain_text' => $description));
+				print_r($update);
+				#print_r($this->update($item->mpid, $update_item));
 			}
 
 		}
@@ -351,5 +481,4 @@ class Meli
 	#echo $t->set_price(34,1049.99);
 	$category = " Productos de oficina, categorías, artículos escolares y de oficina, accesorios de escritorio y organizadores del área de trabajo, alfombrillas para ratón y reposamuñecas, alfombrillas para ratón                                                       ";
 	#$category_id = $t->search_category($category);
-	#print_r($category_id);
 	$t->newItem();
